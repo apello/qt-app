8819448506df755c0a4375377f34899afdc211e1
Abdirahman Nur <corsium.spongea@gmail.com>
2023-07-28 20:09:39 -0400

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import SignIn from "@/components/auth/signIn";
+import SignUp from "@/components/auth/signUp";
+import AuthDetails from "@/components/auth/AuthDetails";
+
+const Home = () => {
+    return (
+        <div>Hello World</div>
+    );
+}
+
+export default Home;

89f93a365e0b121f41c9d2d042c4ca5d69442c6d
Abdirahman Nur <corsium.spongea@gmail.com>
2023-09-23 18:22:59 -0400

    Browser title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../styles/global.css';
 import { CssBaseline, CssVarsProvider } from '@mui/joy';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
       <CssVarsProvider>
+          <Head>
+            <title>QT App</title>
+          </Head>
           <CssBaseline  />
           <Component {...pageProps} />
       </CssVarsProvider>
